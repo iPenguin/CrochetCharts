aba69432fd9c571dcb9f5bdcb521cb1fc3a3c101
Brian C. Milco <bcmilco@gmail.com>
2010-12-30 18:58:57 -0800

    Updated the tests for the new licensing scheme

--- tests/testlicense.cpp
+++ tests/testlicense.cpp
@@ -50,23 +50,26 @@ void TestLicense::checkLicenseNumbers_data()
     QTest::addColumn<QString>("license");
     QTest::addColumn<bool>("valid_license");
 
-    QTest::newRow("one")            << "0000-001-0203" << "bcmilco@gmail.com" << "C539-8507-7080-L1" << true;
-    QTest::newRow("ten")            << "0000-010-9313" << "bcmilco@gmail.com" << "EEAA-4285-888D-L1" << true;
-    QTest::newRow("one hundred")    << "0000-100-5356" << "bcmilco@gmail.com" << "568C-5F60-C9B4-L1" << true;
-    QTest::newRow("one thousand")   << "0001-000-9650" << "bcmilco@gmail.com" << "AAAD-F075-F9FB-L1" << true;
-    QTest::newRow("ten thousand")   << "0010-000-7097" << "bcmilco@gmail.com" << "2957-D7F8-8563-L1" << true;
-    QTest::newRow("one-h thousand") << "0100-000-5861" << "bcmilco@gmail.com" << "B18B-F971-0DB9-L1" << true;
-    QTest::newRow("one million")    << "1000-000-5179" << "bcmilco@gmail.com" << "54AB-41B9-E2E0-L1" << true;
-    QTest::newRow("9,999,999")      << "9999-999-7752" << "bcmilco@gmail.com" << "DDAA-2D64-DC52-L1" << true;
+    QTest::newRow("one")            << "0000-001-0203" << "bcmilco@gmail.com" << "8B14-D834-3FC1-L1" << true;
+    QTest::newRow("ten")            << "0000-010-9313" << "bcmilco@gmail.com" << "360E-ED3D-9BA0-L1" << true;
+    QTest::newRow("one hundred")    << "0000-100-5356" << "bcmilco@gmail.com" << "58BC-AF41-B16A-L1" << true;
+    QTest::newRow("one thousand")   << "0001-000-9650" << "bcmilco@gmail.com" << "ECC0-A35B-5C35-L1" << true;
+    QTest::newRow("ten thousand")   << "0010-000-7097" << "bcmilco@gmail.com" << "90A8-33FE-FF99-L1" << true;
+    QTest::newRow("one-h thousand") << "0100-000-5861" << "bcmilco@gmail.com" << "D049-B534-AD04-L1" << true;
+    QTest::newRow("one million")    << "1000-000-5179" << "bcmilco@gmail.com" << "051D-F96B-26EF-L1" << true;
+    QTest::newRow("9,999,999")      << "9999-999-7752" << "bcmilco@gmail.com" << "00B8-7C1E-0676-L1" << true;
 
-    QTest::newRow("trial - gmail")   << "" << "bcmilco@gmail.com" << "3940-3497-BC8E-T1" << true;
-    QTest::newRow("trial - hotmail") << "" << "bcmilco@hotmail.com" << "3940-3497-BC8E-T1" << true;
-    QTest::newRow("trial - sws")     << "" << "brian@stitchworkssoftware.com" << "3940-3497-BC8E-T1" << true;
+    QTest::newRow("trial - gmail")   << "" << "bcmilco@gmail.com" << "7CC0-1DB8-641B-T1" << true;
+    QTest::newRow("trial - hotmail") << "" << "bcmilco@hotmail.com" << "4827-F9BD-8DF2-T1" << true;
+    QTest::newRow("trial - sws")     << "" << "brian@stitchworkssoftware.com" << "1E3B-18CE-2ED1-T1" << true;
 
-    QTest::newRow("L1 invalid - license");
-    QTest::newRow("L1 invalid - email");
-    QTest::newRow("L1 invalid - serial number");
-    QTest::newRow("T1 invalid");
+    QTest::newRow("L1 invalid - license")  << "0000-001-0203" << "bcmilco@gmail.com" << "C539-8507-1102-L1" << false;
+    QTest::newRow("L1 invalid - email")    << "0000-001-0203" << "bcmilco@gmail" << "C539-8507-7080-L1" << false;
+    QTest::newRow("L1 invalid - sn")       << "0000-001-0533" << "bcmilco@gmail.com" << "C539-8507-7080-L1" << false;
+
+    QTest::newRow("T1 invalid - license")  << "" << "bcmilco@gmail.com" << "C539-8105-7080-L1" << false;
+    QTest::newRow("T1 invalid - email")    << "" << "bcmilco@gmcom" << "C539-8507-7080-L1" << false;
+    QTest::newRow("T1 invalid - sn")       << "0000-001-0533" << "bcmilco@gmail.com" << "C539-8507-7080-L1" << false;
 }
 
 void TestLicense::cleanupTestCase()
