0eabd128eef97103fdf4f2881d0f62516775f57b
Brian C. Milco <bcmilco@gmail.com>
2011-01-01 17:40:52 -0800

    re-order the help menu

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "#include \"../src/version.h\"" > build/version.cpp
-echo "#define GIT_VERSION" \"`git describe --tags --dirty`\" >> build/version.cpp
+echo "#define GIT_VERSION" \"`git describe --tags --dirty=-working`\" >> build/version.cpp
 echo "const QString gGIT_VERSION = GIT_VERSION;" >> build/version.cpp
 
