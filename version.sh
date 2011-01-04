e1da2d2bec604435f74d0f21572f20f2464addde
Brian C. Milco <bcmilco@gmail.com>
2011-01-04 10:58:31 -0800

    added xml header and deleted extra line

--- version.sh
+++ version.sh
@@ -2,4 +2,3 @@
 echo "#include \"../src/version.h\"" > build/version.cpp
 echo "#define GIT_VERSION" \"`git describe --tags --dirty=-working`\" >> build/version.cpp
 echo "const QString gGIT_VERSION = GIT_VERSION;" >> build/version.cpp
-
