82b7ad555507c63dfc5991ba6dc4b115b9cf675e
Brian C. Milco <bcmilco@gmail.com>
2011-02-07 17:42:32 -0800

    Basic commit with new stitch icon ui files

---
+++ src/stitchiconui.cpp
@@ -0,0 +1,13 @@
+/*************************************************\
+| Copyright (c) 2011 Stitch Works Software        |
+| Brian C. Milco <brian@stitchworkssoftware.com>  |
+\*************************************************/
+#include "stitchiconui.h"
+
+StitchIconUi::StitchIconUi()
+{
+}
+
+StitchIconUi::~StitchIconUi()
+{
+}
---
+++ src/stitchiconui.h
@@ -0,0 +1,20 @@
+/*************************************************\
+| Copyrigth (c) 2011 Stitch Works Software        |
+| Brian C. Milco <brian@stitchworkssoftware.com>  |
+\*************************************************/
+#ifndef STITCHICONUI_H
+#define STITCHICONUI_H
+
+#include <QDialog>
+
+class StitchIconUi : public QDialog
+{
+    Q_OBJECT
+public:
+    StitchIconUi();
+    ~StitchIconUi();
+
+private:
+
+};
+#endif //STITCHICONUI_H
