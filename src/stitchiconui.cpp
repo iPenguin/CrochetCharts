2747f92049fa8a2aa7c512f0bd8cbdd7ccd367f2
Brian C. Milco <bcmilco@gmail.com>
2011-02-18 16:15:49 -0800

    Only show the user added icons in the ui.

--- src/stitchiconui.cpp
+++ src/stitchiconui.cpp
@@ -33,18 +33,21 @@ void StitchIconUi::loadIcons()
     QStringList dirs, setDir;
     QString userFolder = Settings::inst()->userSettingsFolder();
 
-    dirs << ":/stitches";
+
     dirs << userFolder + "icons";
 
     QDir dir;
+/*
     dir.setPath(userFolder + "sets/");
 
+    dirs << ":/stitches";
+    
     //get all set folders.
     foreach(QString folder, dir.entryList(QDir::Dirs)) {
         if(folder != "." && folder != "..")
             dirs << userFolder + "sets/" + folder;
     }
-
+*/
     //get all files from all set folders.
     foreach(QString folder, dirs) {
         dir.setPath(folder);
