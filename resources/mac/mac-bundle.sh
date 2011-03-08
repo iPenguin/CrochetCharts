74481f0a983899bdfdbccef0182d8c9a5a2a7cc1
Brian C. Milco <bcmilco@gmail.com>
2011-03-07 17:01:23 -0800

    Major rework of cpack files. Also cmake files

--- resources/mac/mac-bundle.sh
+++ resources/mac/mac-bundle.sh
@@ -3,11 +3,9 @@
 
 #$1 is the name of the package to make (x.app, MacOS/x, x.icns, etc)
 #$2 is the Mach-O binary
-#$3 is the Info.plist (does not have to be called info.plist, will be
-renamed)
+#$3 is the Info.plist (does not have to be called info.plist, will be renamed)
 #$4 is the binary.icns file
-#$5 $6 ... and on are paths to .frameworks (or .dylib files) that need
-to be bundled with the app
+#$5 $6 ... and on are paths to .frameworks (or .dylib files) that need to be bundled with the app
 
 if [ "x$4" == "x" ]; then
 {
@@ -23,7 +21,7 @@ else
     mkdir ${BUNDLE}.app/Contents/MacOS
     mkdir ${BUNDLE}.app/Contents/Resources
     mkdir ${BUNDLE}.app/Contents/Frameworks
-    #mkdir ${BUNDLE}.app/Contents/PlugIns
+    mkdir ${BUNDLE}.app/Contents/PlugIns
     cp $2 ${BUNDLE}.app/Contents/MacOS/
     cp $3 ${BUNDLE}.app/Contents/Info.plist
     cp $4 ${BUNDLE}.app/Contents/Resources/
