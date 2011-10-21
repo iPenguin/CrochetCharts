a1a71925d5eeee6e1ace4d6b726388a1a38e6a69
Brian C. Milco <bcmilco@gmail.com>
2011-10-20 20:28:33 -0700

    Remove the server files from the folder structure.

--- server/test-license-functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("license-functions.php");
-print "<pre><br />";
-print generate_license("0000-001-0533","brian@gmail.com")."<br />";
-print generate_license("","brian@gmail.com")."<br />";
-
-print "</pre><br />";
-?>
