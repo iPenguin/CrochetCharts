4768c68dcad93dfb3712a324ef7895d685a43657
Brian C. Milco <bcmilco@gmail.com>
2011-11-15 01:17:50 -0800

    Make sure when we're compiling for 32bit that the packaging matches.

--- resources/installers.cmake
+++ resources/installers.cmake
@@ -122,23 +122,25 @@ elseif(APPLE)
 
 else()
 
-    set(CPACK_GENERATOR "DEB;RPM;STGZ;TBZ2")
+    set(CPACK_GENERATOR "DEB") #;RPM;STGZ;TBZ2")
 
     set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Brian Milco <${PROJECT_CONTACT}>")
-    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6, libqtgui4, libqtcore4, libqt4-svg, libqt4-xml, libqt4-network")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6, libqtgui4 (>= 4.7.0), libqtcore4 (>= 4.7.0), libqt4-svg (>= 4.7.0), libqt4-xml (>= 4.7.0), libqt4-network (>= 4.7.0)")
     set(CPACK_DEBIAN_PACKAGE_SECTION "Graphics")
     set(CPACK_DEBIAN_PACKAGE_VERSION ${CPACK_PACKAGE_VERSION})
 
 
     set(CPACK_RPM_PACKAGE_LICENSE "Commercial")
     set(CPACK_RPM_PACKAGE_GROUP "Applications/Productivity")
-    set(CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
     set(CPACK_RPM_PACKAGE_VENDOR ${CPACK_PACKAGE_VENDOR})
     set(CPACK_RPM_PACKAGE_REQUIRES "requires: libc6, qt >= 4.7, qt-x11 >= 4.7")
 
     if(FORCE_32BIT)
         set(CPACK_RPM_PACKAGE_ARCHITECTURE "i386")
-        set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
+        set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
+    else()
+        set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
+        set(CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
     endif()
     
 endif()
