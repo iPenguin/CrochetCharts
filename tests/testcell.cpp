287e397b87a332ea2ff0449ce9b41631dd699b07
Brian C. Milco <bcmilco@gmail.com>
2011-12-27 13:52:57 -0800

    Refs #12 - add tests to the cell for all properties. clean up tests

--- tests/testcell.cpp
+++ tests/testcell.cpp
@@ -12,11 +12,8 @@
 void TestCell::initTestCase()
 {
     i = 0;
-    scene = new QGraphicsScene();
-    view = new QGraphicsView(scene);
 
     StitchLibrary::inst()->loadStitchSets();
-    mCell = new Cell();
 }
 
 void TestCell::setCellValues()
@@ -25,11 +22,15 @@ void TestCell::setCellValues()
     QFETCH(qreal, width);
     QFETCH(qreal, height);
 
+    Cell* c = new Cell();
     Stitch* s = StitchLibrary::inst()->findStitch(name);
-    mCell->setStitch(s);
+    c->setStitch(s);
 
-    QVERIFY(mCell->sceneBoundingRect().width() == width);
-    QVERIFY(mCell->sceneBoundingRect().height() == height);
+    QVERIFY(c->sceneBoundingRect().width() == width);
+    QVERIFY(c->sceneBoundingRect().height() == height);
+
+    delete c;
+    c = 0;
 
 }
 
@@ -54,26 +55,29 @@ void TestCell::setRotation()
     QFETCH(qreal, angle);
     QFETCH(QString, rasterHash);
 
-    scene->addItem(mCell);
+    Cell* c = new Cell();
+    QGraphicsScene *scene = new QGraphicsScene();
+    scene->addItem(c);
     i++;
 
     Stitch* s = StitchLibrary::inst()->findStitch(name);
-    mCell->setStitch(s);
+    c->setStitch(s);
 
-    QPointF origin = QPointF(mCell->boundingRect().width()/2, mCell->boundingRect().height());
-    mCell->setTransformOriginPoint(origin);
-    mCell->setRotation(angle);
+    QPointF origin = QPointF(c->boundingRect().width()/2, c->boundingRect().height());
+    c->setTransformOriginPoint(origin);
+    c->setRotation(angle);
 
-    QString rasterImage = "TestCell-SetRotation-" + name + "-" + QString::number(i) + ".png";
+    QString rasterImage = "TestCell-SetRotation-" + QString::number(i) + "-" + name + ".png";
 
-    saveScene(QSizeF(width, height), rasterImage);
+    saveScene(scene, QSizeF(width, height), rasterImage);
 
     QString hexHash = hashFile(rasterImage);
 
     QCOMPARE(hexHash, rasterHash);
 
-    mCell->setRotation(0);
-    scene->removeItem(mCell);
+    scene->removeItem(c);
+    delete c;
+    c = 0;
 }
 
 void TestCell::setRotation_data()
@@ -86,8 +90,8 @@ void TestCell::setRotation_data()
     QTest::addColumn<QString>("rasterHash");
 
     QTest::newRow("ch")    << "ch" << 32.0 << 16.0 << 45.0 << "3acb54f2d0d610851417e9a15fa5bcf56c5ca934";
-    QTest::newRow("hdc")   << "hdc" << 32.0 << 64.0 << 45.0 << "8810b0554ae658d526d742512b8fa6b4c9c5039d";
-    QTest::newRow("dc")    << "dc" << 32.0 << 80.0 << 45.0 << "525c69657503c470995b73633c096b185f0390bc";
+    QTest::newRow("hdc")   << "hdc" << 32.0 << 64.0 << 45.0 << "a1d58264e09bfe2583a4053ebb9753332c1be217";
+    QTest::newRow("dc")    << "dc" << 32.0 << 80.0 << 45.0 << "b76157343c5e9b71b519bfba0d013275edfa8d98";
 
 }
 
@@ -101,27 +105,30 @@ void TestCell::setScale()
     QFETCH(qreal, scaleY);
     QFETCH(QString, rasterHash);
 
-    scene->addItem(mCell);
+    Cell* c = new Cell();
+    QGraphicsScene *scene = new QGraphicsScene();
+
+    scene->addItem(c);
     i++;
 
     Stitch* s = StitchLibrary::inst()->findStitch(name);
-    mCell->setStitch(s);
+    c->setStitch(s);
 
-    QPointF origin = QPointF(mCell->boundingRect().width()/2, mCell->boundingRect().height());
-    mCell->setTransformOriginPoint(origin);
-    mCell->setScale(scaleX, scaleY);
+    QPointF origin = QPointF(c->boundingRect().width()/2, c->boundingRect().height());
+    c->setTransformOriginPoint(origin);
+    c->setScale(scaleX, scaleY);
 
-    QString rasterImage = "TestCell-ScaleTest-" + name + "-" + QString::number(i) + ".png";
+    QString rasterImage = "TestCell-ScaleTest-" + QString::number(i) + "-" + name + ".png";
 
-    saveScene(QSizeF(width, height), rasterImage);
+    saveScene(scene, QSizeF(width, height), rasterImage);
 
     QString hexHash = hashFile(rasterImage);
 
     QCOMPARE(hexHash, rasterHash);
 
-    mCell->setScale(1.0, 1.0);
-    scene->removeItem(mCell);
-
+    scene->removeItem(c);
+    delete c;
+    c = 0;
 }
 
 void TestCell::setScale_data()
@@ -134,9 +141,9 @@ void TestCell::setScale_data()
     QTest::addColumn<qreal>("scaleY");
     QTest::addColumn<QString>("rasterHash");
 
-    QTest::newRow("ch")    << "ch" << 32.0 << 16.0 << 1.0 << 1.0 << "97999ab9dd7ae38d62026c72e26dbae3757bf8ae";
-    QTest::newRow("hdc")   << "hdc" << 32.0 << 64.0 << 2.5 << 2.5 << "4986b9226e512c9ac05108187d66c8352501a298";
-    QTest::newRow("dc")    << "dc" << 32.0 << 80.0 << 2.5 << 2.5 << "e2d1d603695e924a124ff0685be98153872da642";
+    QTest::newRow("ch")    << "ch" << 32.0 << 16.0 << 1.0 << 1.0 << "0be6578d22717ba56082a408e8ed128e8114a30c";
+    QTest::newRow("hdc")   << "hdc" << 32.0 << 64.0 << 2.5 << 2.5 << "090bf8ac828e1d2ad0da3ec240539276148bc134";
+    QTest::newRow("dc")    << "dc" << 32.0 << 80.0 << 2.5 << 2.5 << "bdb70eea2145b79c7cb4e50a6148f5ec4d09f708";
 }
 
 void TestCell::setBgColor()
@@ -148,26 +155,29 @@ void TestCell::setBgColor()
     QFETCH(QString, color);
     QFETCH(QString, rasterHash);
 
-    scene->addItem(mCell);
+    QGraphicsScene *scene = new QGraphicsScene();
+    Cell* c = new Cell();
+    scene->addItem(c);
     i++;
 
     Stitch* s = StitchLibrary::inst()->findStitch(name);
-    mCell->setStitch(s);
+    c->setStitch(s);
 
-    QPointF origin = QPointF(mCell->boundingRect().width()/2, mCell->boundingRect().height());
-    mCell->setTransformOriginPoint(origin);
-    mCell->setColor(QColor(color));
+    QPointF origin = QPointF(c->boundingRect().width()/2, c->boundingRect().height());
+    c->setTransformOriginPoint(origin);
+    c->setColor(QColor(color));
 
-    QString rasterImage = "TestCell-BgColor-" + name + "-" + QString::number(i) + ".png";
+    QString rasterImage = "TestCell-BgColor-" + QString::number(i) + "-" + name + ".png";
 
-    saveScene(QSizeF(width, height), rasterImage);
+    saveScene(scene, QSizeF(width, height), rasterImage);
 
     QString hexHash = hashFile(rasterImage);
 
     QCOMPARE(hexHash, rasterHash);
 
-    mCell->setColor();
-    scene->removeItem(mCell);
+    scene->removeItem(c);
+    delete c;
+    c = 0;
 }
 
 void TestCell::setBgColor_data()
@@ -179,14 +189,74 @@ void TestCell::setBgColor_data()
     QTest::addColumn<QString>("color");
     QTest::addColumn<QString>("rasterHash");
 
-    QTest::newRow("ch")    << "ch" << 32.0 << 16.0 << "#0000FF" << "9784f1d2220b56081cc3292fe7c9fb215d72a736";
-    QTest::newRow("hdc")   << "hdc" << 32.0 << 64.0 << "#FF0000" << "4664a9531fd5d365e2abec8be1d767f716b74412";
-    QTest::newRow("dc")    << "dc" << 32.0 << 80.0 << "#00FF00" << "16fb469cbc74011c59c4a8363af7eac50447197e";
+    QTest::newRow("ch")    << "ch" << 32.0 << 16.0 << "#0000FF" << "9d6c0ac6e51809f662573c9432c0b5c7064c2021";
+    QTest::newRow("hdc")   << "hdc" << 32.0 << 64.0 << "#FF0000" << "c3105395f5d7e6ad8269208d25e8be76ab7a9455";
+    QTest::newRow("dc")    << "dc" << 32.0 << 80.0 << "#00FF00" << "0a4fedb0079e244671ab5a376984f40df3394b94";
 
 }
 
-void TestCell::saveScene(QSizeF size, QString fileName)
+void TestCell::setAllProperties()
 {
+
+    QFETCH(QString, name);
+    QFETCH(qreal, width);
+    QFETCH(qreal, height);
+    QFETCH(qreal, angle);
+    QFETCH(qreal, scaleX);
+    QFETCH(qreal, scaleY);
+    QFETCH(QString, color);
+    QFETCH(QString, rasterHash);
+
+    QGraphicsScene *scene = new QGraphicsScene();
+    Cell* c = new Cell();
+    scene->addItem(c);
+    i++;
+
+    Stitch* s = StitchLibrary::inst()->findStitch(name);
+    c->setStitch(s);
+
+    QPointF origin = QPointF(c->boundingRect().width()/2, c->boundingRect().height());
+    c->setTransformOriginPoint(origin);
+    c->setColor(QColor(color));
+    c->setRotation(angle);
+    c->setScale(scaleX, scaleY);
+
+
+    QString rasterImage = "TestCell-AllProperties-" + QString::number(i) + "-" + name + ".png";
+
+    saveScene(scene, QSizeF(width, height), rasterImage);
+
+    QString hexHash = hashFile(rasterImage);
+
+    QCOMPARE(hexHash, rasterHash);
+
+    scene->removeItem(c);
+    delete c;
+    c = 0;
+}
+
+void TestCell::setAllProperties_data()
+{
+
+    QTest::addColumn<QString>("name");
+    QTest::addColumn<qreal>("width");
+    QTest::addColumn<qreal>("height");
+    QTest::addColumn<qreal>("angle");
+    QTest::addColumn<qreal>("scaleX");
+    QTest::addColumn<qreal>("scaleY");
+    QTest::addColumn<QString>("color");
+    QTest::addColumn<QString>("rasterHash");
+
+    QTest::newRow("ch")    << "ch" << 32.0 << 16.0 << 45.0 << 1.5 << 2.0 << "#0000FF" << "2d56f4703b3f29f1163bdd6197597e3018a004e6";
+    QTest::newRow("hdc")   << "hdc" << 32.0 << 64.0 << 45.0 << 1.5 << 2.0 << "#FF0000" << "2bffe89a7dad96f18eae2ded0dc08f7f207ceb30";
+    QTest::newRow("dc")    << "dc" << 32.0 << 80.0 << 45.0 << 1.5 << 2.0 << "#00FF00" << "0c151d2b0aee5157a59e1ac769bb26644365ced9";
+
+}
+
+void TestCell::saveScene(QGraphicsScene* scene, QSizeF size, QString fileName)
+{
+
+    scene->setSceneRect(scene->itemsBoundingRect());
 
     QPixmap pix = QPixmap(scene->sceneRect().width(), scene->sceneRect().height());
     pix.fill(QColor(Qt::white));
