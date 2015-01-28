236aa1da3f93d93c243a0eb5fb1108614584ba09
Brian C. Milco <bcmilco@gmail.com>
2015-01-28 00:28:21 -0800

    Update licensing to GPLv3. Update basic README file.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Crochet Charts
+
+## Setup a working environment
+
+To use this project you should first setup the repository for use, at this point the
+setup script only sets up commit hooks for git.
+
+./bin/seutp
+
+Once the process is complete you can begin working on the software.
+
+For ease of use there are some scripts in the bin folder that can help with development process.
+
+- build   - compile the software based on the selected options.
+- tests   - run the unit tests and report the results in an easy to read format.
+- profile - run the software with profiling turned on.
+
+
+
--- src/application.cpp
+++ src/application.cpp
@@ -1,3 +1,23 @@
+/****************************************************************************\
+ Copyright (c) 2010-2014 Stitch Works Software
+ Brian C. Milco <bcmilco@gmail.com>
+
+ This file is part of Crochet Charts.
+
+ Crochet Charts is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Crochet Charts is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Crochet Charts. If not, see <http://www.gnu.org/licenses/>.
+
+ \****************************************************************************/
 #include "application.h"
 
 #include "appinfo.h"
