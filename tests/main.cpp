9dd23de5b0872680d7e7a0d2a8d6307f411ad376
Brian C. Milco <bcmilco@gmail.com>
2015-02-09 23:04:32 -0800

    Update license for test source files. Remove licensing test class.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,24 @@
-/*************************************************\
-| Copyright (c) 2010 Stitch Works Software        |
-| Brian C. Milco <brian@stitchworkssoftware.com>  |
-\*************************************************/
+/****************************************************************************\
+ Copyright (c) 2010-2014 Stitch Works Software
+ Brian C. Milco <bcmilco@gmail.com>
+
+ This file is part of Crochet Charts.
+
+ Crochet Charts is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Crochet Charts is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Crochet Charts. If not, see <http://www.gnu.org/licenses/>.
+
+ \****************************************************************************/
 #include "testsettings.h"
-#include "testlicense.h"
 #include "teststitch.h"
 #include "teststitchset.h"
 #include "testcell.h"
@@ -18,10 +33,6 @@ int main(int argc, char** argv)
 
     QObject* test;
     qDebug() << "All output files are in the build directory";
-    test = new TestLicense();
-    retval +=QTest::qExec(test, argc, argv);
-    delete test;
-    test = 0;
 
     //dep: license (initDemo)
     test = new TestSettings();
